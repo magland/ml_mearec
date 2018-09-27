a0fdcf1a2a24405105330e100e43294c9c638a6b
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-27 10:13:29 -0400

    fix deploy auth and incr version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ pkg_name='ml_mearec'
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.2",
+    version="0.1.3",
     author="Jeremy Magland",
     author_email="",
     description="MountainLab wrapper for MEArec",

1af48168fc6e7f50d200cd67a316ec8c75317471
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-28 09:15:39 -0400

    incr version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ pkg_name='ml_mearec'
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.6",
+    version="0.1.7",
     author="Jeremy Magland",
     author_email="",
     description="MountainLab wrapper for MEArec",
